e35cbd325c33bbd8ce22fb619baa30ca88e06a39
Ajay <ajayvattipally@gmail.com>
2015-03-28 20:48:36 +0000

    commited first file

---
+++ first.py
@@ -0,0 +1,6 @@
+#Python script to find out if entered number is even
+num = raw_input("Enter a number: ")
+if num%2 == 0:
+    print "Entered number is Even!"
+else:
+    print "Entered number is Odd!"
